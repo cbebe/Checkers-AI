40692460374ec64855b27c6d111d5c6b83506a72
charlesancheta <cancheta@ualberta.ca>
2020-03-17 14:04:06 -0600

    ahh

--- arduino_ui/checkers.cpp
+++ arduino_ui/checkers.cpp
@@ -11,9 +11,43 @@
 // display init
 MCUFRIEND_kbv tft;
 
+#define BOARD_DARK 19458 // colours for game board
+#define BOARD_LIGHT 57113 // please change i don't like
+#define BOARD_SQUARE 35
+
+struct Piece {
+  uint8_t colour;
+  bool king;
+  uint8_t position; // position of the piece from 1 to 32
+}
+
+void drawPiece(Piece piece) {
+
+}
+
+void gameStart() {
+  Piece white = ;
+  Piece black;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+
+      drawPiece() 
+    }
+  }
+  
+}
 
 void drawBoard() {
-  tft.fillRect(0,0)
+  tft.fillRect(100,20,280,280, BOARD_DARK);
+
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      tft.fillRect(100 + i*70, 20 + j*70, BOARD_SQUARE, BOARD_SQUARE, BOARD_LIGHT);
+      tft.fillRect(135 + i*70, 55 + j*70, BOARD_SQUARE, BOARD_SQUARE, BOARD_LIGHT);
+
+    }
+  }
+  tft.println(BOARD_LIGHT);
 }
 
 
@@ -42,6 +76,7 @@ int main() {
   enum {MENU, GAME_START, GAME_END} mode = MENU;
 
   drawBoard();
+  gameStart();
   while (true) {
 
   }
---
+++ resources/tft-lib-demo/build-mega2560/tftdemo.cpp.d
@@ -0,0 +1,16 @@
+build-mega2560/tftdemo.cpp.o: tftdemo.cpp \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino//variants/mega/pins_arduino.h \
+ /home/charles/arduino-ua/libraries/Adafruit_GFX/Adafruit_GFX.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /home/charles/arduino-ua/libraries/Adafruit_GFX/gfxfont.h \
+ /home/charles/arduino-ua/libraries/Adafruit_GFX/Fonts/FreeSansBold9pt7b.h \
+ /home/charles/arduino-ua/libraries/MCUFRIEND_kbv/MCUFRIEND_kbv.h
